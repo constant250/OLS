27e5f95d12ecac62d7a21241843462d0b57225c4
Kevin Jay Adaya <kjadaya30@gmail.com>
2020-01-25 12:10:59 +0800

    James First Commit 01-25-2020

---
+++ resources/js/components/category/categoryListComponent.vue
@@ -0,0 +1,181 @@
+<template>
+    <div class="app-modal">
+        <create-project/>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-info">Equipment List</h6>
+            </div>
+            <div class="card-body">
+                <v-client-table :data="CategoryList" :columns="columns" :options="options" ref="courseTable">
+                        <div slot="afterLimit" class="ml-2">
+                            <div class="btn-group">
+                                <a href="javascript:void(0)"  @click="showCreateCategory" class="btn btn-info" slot="afterLimit"><i class="fas fa-plus"></i> Add Category</a>
+                                <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Export to
+                                    <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="#"><i class="far fa-file-pdf text-danger"></i>&nbsp; PDF</a>
+                                    <a class="dropdown-item" href="#"><i class="far fa-file-excel text-success"></i>&nbsp; Excel (CSV)</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="btn-group" slot="actions" slot-scope="{row}">
+                            <a href="javascript:void(0)" class="btn btn-primary btn-sm" @click="equipDetail(row.id)"> <i class="fas fa-edit"> </i></a>
+                            <a href="javascript:void(0)" class="btn btn-danger btn-sm text-white" @click="removeEquip(row.id)"> <i class="fas fa-trash"> </i></a>
+                        </div>
+                </v-client-table>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+    import CreateProject from './createProjectComponent.vue'
+
+    export default {
+        name: 'app-modal',
+        // mounted() {
+        //     console.log('Component mounted.')
+        // }
+        components: {
+            CreateProject
+        },
+        data() {
+            return {
+                projectList : [],
+                project : {},
+                project_id : '',
+                pagination : {},
+                edit: false,
+                searchProjectName: '',
+                url: 'project/show/',
+
+                // Vue-Tables-2 Syntax
+                columns: ['id', 'name', 'project_type.name', 'user.party.name', 'actions'],
+                options: {
+                    initialPage:1,
+                    perPage:10,
+                    highlightMatches:true,
+                    sortIcon: { base:'fas', up:'fa-sort-amount-up', down:'fa-sort-amount-down', is:'fa-sort' },
+                    headings: {
+                        id: '#',
+                        name: 'Project Name',
+                        'project_type.name': 'Project Type',
+                        'user.party.name': 'User',
+                        actions: 'Actions'
+                    },
+                    sortable: ['name', 'project_type.name', 'user.party.name'],
+                    rowClassCallback(row) {
+                        return row.id = row.id;
+                    },
+                    columnClasses: {id: 'class-is'},
+                    texts: {
+                        // filter: "Search:",
+                        filterPlaceholder: "Search keywords",
+                    }
+                },
+
+            };
+        },
+        created() {
+            this.fetchProject();
+        },
+        methods: {
+            // searchProject() {
+            //     let vm = this;
+            //     console.log(this.searchProjectName);
+            //     let search = (this.searchProjectName ? this.searchProjectName : '');
+
+            //     fetch('/project/list/search/'+search)
+            //         .then(res => res.json())
+            //         .then(res => {
+            //             this.projectList = res.data;
+            //             vm.makePagination(res);
+            //         })
+            //         .catch(err => console.log(err));
+            // },
+            fetchProject(page_url) {
+                let vm = this;
+                page_url = page_url || '/proj/list'
+                fetch(page_url)
+                    .then(res => res.json())
+                    .then(res => {
+                        console.log(res);
+                        this.projectList = res;
+                        vm.makePagination(res);
+                    })
+                    .catch(err => console.log(err));
+            },
+            makePagination(meta){
+                let pagination = {
+                    current_page: meta.current_page,
+                    last_page: meta.last_page,
+                    next_page_url: meta.next_page_url,
+                    prev_page_url: meta.prev_page_url
+                }
+                this.pagination = pagination
+            },
+            showCreateProject () {
+                this.$modal.show('size-modal',{
+                    edit : false,
+                    id : '',
+                    course_id : '',
+                    name : '',
+                    code : '',
+                })
+            },
+            equipDetail (id) {
+                window.location.href = '/project/'+id;
+            },
+            removeEquip(id) {
+                let vm = this;
+                swal.fire({
+                    title: 'Are you sure delete project?',
+                    text: " this won't be able to revert!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.value) {
+                        axios.delete('/project/'+id)
+                        .then(function(res){
+                        // vm.files = res.data;
+                        if(res.data.status == 'success'){
+                            swal.fire(
+                                'Deleted!',
+                                'Equipment has been deleted.',
+                                'success'
+                            )
+                            vm.fetchProject();
+                        }
+                        })
+                        .catch(function(error){
+                        });
+                    }
+                });
+            }
+  
+            
+        }
+    }
+</script>
+
+<style>
+    th{
+        background-color: #36b9cc;
+        text-align: center;
+        color: #fff;
+        border:1px solid #36b9cc !important;
+        vertical-align: middle !important;
+    }
+    td.text-center{
+        border-right-color: #fff !important;
+        border-left-color: #fff !important;
+        vertical-align: middle !important;
+    }
+</style>
---
+++ resources/js/james.js
@@ -0,0 +1,3 @@
+// Projects
+Vue.component('category-list', require('./components/category/categoryListComponent.vue').default);
+// Vue.component('project-detail', require('./components/project/projectDetailComponent.vue').default);
