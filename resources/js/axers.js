c2b8f8546d9193445d4ef6789463af5ada0e1a2c
Axers15 <xenemax6@gmail.com>
2020-02-20 21:29:59 +0800

    Commit Axers 02-20-20

--- resources/js/axers.js
+++ resources/js/axers.js
@@ -1,3 +1,4 @@
 // Discipline
 Vue.component('discipline-list', require('./components/discipline/disciplineListComponent.vue').default);
 // Vue.component('project-detail', require('./components/project/projectDetailComponent.vue').default);
+Vue.component('discipline-detail', require('./components/discipline/disciplineDetailComponent.vue').default);
---
+++ resources/js/components/discipline/disciplineDetailComponent.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+    
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-info">Discipline Details</h6>
+        </div>
+        <div class = "card-body">
+            <dynamic-form
+            v-bind:form-settings="makeForm"
+            v-bind:form-values="getValues"
+            v-bind:save-form="'/discipline'"
+            v-bind:back-route="'/discipline'"
+            ></dynamic-form>
+        </div>
+    </div>
+    </div>
+</template>
+
+
+<script>
+import DynamicForm from "./../globals/form/DynamicFormComponent.vue"
+
+export default {
+
+    components: {
+        DynamicForm
+    },
+    mounted(){
+        this.csrfToken=document.querySelector('meta[name="csrf-token"]').content
+    },
+    data(){
+        return{
+            csrfToken:'',
+            makeForm: [{
+                FormTitle : 'Family Information',
+                FormBody : [
+                    {
+                        type : 'text',
+                        label : 'Family Name',
+                        name : 'name',
+                    }
+                ]
+
+            }],
+            getValues:window.discipline,
+        }
+    }
+
+}
+
+</script>
+
+<style>
+
+.horizontal-line-wrapper h6{
+    color: #fff;
+    font-size: 16px;
+    padding: 10px;
+    font-weight: 700;
+}
+</style>
